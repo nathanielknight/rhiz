c5ef1f19a2a19391ee045e4ae846121f542c6c4e
Nat Knight <nathaniel.ep@gmail.com>
2019-02-11 11:38:11 -0800

    Test of basic compilation and execution

---
+++ tests/test_basic.rs
@@ -0,0 +1,11 @@
+use rhiz::ast::parse_rhiz_program;
+use rhiz::compiler::compile;
+use rhiz::executor::exec_task;
+
+#[test]
+fn test_basic_compilation_and_execution() {
+    let src = r#"(task "dummy-task" (log "This is a dummy task"))"#;
+    let parsed = parse_rhiz_program(src).unwrap();
+    let compiled = compile(&parsed).unwrap();
+    let execd = exec_task("dummy-task", compiled).unwrap();
+}
