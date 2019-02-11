2ae7c8aaa20ab322bb665e51872b465122dcb663
Nathaniel Knight <nknight@cfenet.ubc.ca>
2019-02-11 15:47:27 -0800

    Add error-with macro with format!

--- src/functions.rs
+++ src/functions.rs
@@ -1,7 +1,30 @@
 use crate::ast::RhizValue;
 use crate::executor::{ExecutionError, ExecutionResult};
 
-type RhizFunction = Fn(Vec<RhizValue>) -> ExecutionResult;
+
+macro_rules! error_with {
+    ($fmt:expr, $($val: expr),*) => {
+        {
+            let msg = format!($fmt, $($val,)*);
+            return Err(ExecutionError::from(msg));
+        }
+    }
+}
+
+#[test]
+fn test_error_with() {
+    fn do_err() -> ExecutionResult {
+        error_with!("oh {}", "noes")
+    }
+    assert!(do_err().is_err());
+    fn do_ok() -> ExecutionResult {
+        if false {
+            error_with!("{} {} {}", "plz", "to", "not");
+        }
+        Ok(())
+    }
+    assert!(do_ok().is_ok());
+}
 
 fn look_up_function(fname: &str) -> Option<Box<RhizFunction>> {
     unimplemented!()
