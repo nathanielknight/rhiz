8ae98dd5e1a788333d71de01c0b6125d716477dc
Nat Knight <nathaniel.ep@gmail.com>
2019-02-11 11:37:12 -0800

    First function implementation
    Change the signature of RhizFunction and implement 'log'

--- src/functions.rs
+++ src/functions.rs
@@ -1,8 +1,42 @@
 use crate::ast::RhizValue;
 use crate::executor::{ExecutionError, ExecutionResult};
 
-type RhizFunction = Fn(Vec<RhizValue>) -> ExecutionResult;
+type RhizFunction = Fn(&[RhizValue]) -> ExecutionResult;
 
-fn look_up_function(fname: &str) -> Option<Box<RhizFunction>> {
-    unimplemented!()
+pub fn look_up_function(func_name: &RhizValue) -> Option<Box<RhizFunction>> {
+    let symbol_name = match func_name {
+        RhizValue::Symbol(s) => s,
+        _ => return None,
+    };
+    match symbol_name.as_ref() {
+        "log" => Some(Box::new(log)),
+        "abort" => Some(Box::new(abort)),
+        _ => None,
+    }
+}
+
+fn log(args: &[RhizValue]) -> ExecutionResult {
+    if args.len() != 1 {
+        return Err(ExecutionError::from("`log` takes one argument"));
+    }
+    match &args[0] {
+        RhizValue::String(s) => {
+            println!("{}", s);
+            Ok(())
+        }
+        _ => Err(ExecutionError::from("`log` takes a string")),
+    }
+}
+
+fn abort(args: &[RhizValue]) -> ExecutionResult {
+    if args.len() != 1 {
+        return Err(ExecutionError::from("`abort` takes one argument"));
+    }
+    match &args[0] {
+        RhizValue::String(s) => {
+            eprintln!("{}", s);
+            Err(ExecutionError::from("Execution aborted"))
+        }
+        _ => Err(ExecutionError::from("`abort` takes a string")),
+    }
 }
