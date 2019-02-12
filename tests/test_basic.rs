feb6b7ecd895f209bd4381fecb31ce1314b54430
Nathaniel Knight <nknight@cfenet.ubc.ca>
2019-02-12 15:20:20 -0800

    Adopt new RhizFunction definition

--- tests/test_basic.rs
+++ tests/test_basic.rs
@@ -7,5 +7,7 @@ fn test_basic_compilation_and_execution() {
     let src = r#"(task "dummy-task" (log "This is a dummy task"))"#;
     let parsed = parse_rhiz_program(src).unwrap();
     let compiled = compile(&parsed).unwrap();
-    exec_task("dummy-task", &compiled).unwrap();
+    let pbuf = std::env::current_dir().unwrap();
+    let cwd = pbuf.as_path();
+    exec_task("dummy-task", &compiled, &cwd).unwrap();
 }
