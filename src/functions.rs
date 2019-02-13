071c3fea6213e142e054defdc9af2e1f67950518
Nathaniel Knight <nknight@cfenet.ubc.ca>
2019-02-12 16:36:56 -0800

    Add an exec function

--- src/functions.rs
+++ src/functions.rs
@@ -1,6 +1,8 @@
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
 use crate::ast::RhizValue;
 use crate::executor::{ExecutionError, ExecutionResult};
-use std::path::Path;
 
 type RhizFunction = Fn(&[RhizValue], &Path) -> ExecutionResult;
 
@@ -28,10 +30,27 @@ pub fn look_up_function(func_name: &RhizValue) -> Option<Box<RhizFunction>> {
         "log" => Some(Box::new(log)),
         "abort" => Some(Box::new(abort)),
         "delete-file" => Some(Box::new(delete_file)),
+        "delete-dir" => Some(Box::new(delete_dir)),
+        "exec" => Some(Box::new(exec)),
         _ => None,
     }
 }
 
+fn join_cwd(cwd: &Path, fpath: &str) -> PathBuf {
+    let mut cwd = cwd.to_path_buf();
+    cwd.push(fpath);
+    cwd
+}
+
+fn val_to_string(rval: &RhizValue) -> Option<String> {
+    match rval {
+        RhizValue::String(s) => Some(s.to_owned()),
+        RhizValue::Symbol(s) => Some(s.to_owned()),
+        _ => None,
+    }
+}
+
+/// Print a message to the console.
 fn log(args: &[RhizValue], _: &Path) -> ExecutionResult {
     check_args_len!("log", args, 1);
     let msg = if let RhizValue::String(s) = &args[0] {
@@ -43,6 +62,7 @@ fn log(args: &[RhizValue], _: &Path) -> ExecutionResult {
     Ok(())
 }
 
+/// Stop executing the Rhizfile, printing an error message.
 fn abort(args: &[RhizValue], _: &Path) -> ExecutionResult {
     check_args_len!("abort", args, 1);
     let msg = if let RhizValue::String(s) = &args[0] {
@@ -54,6 +74,7 @@ fn abort(args: &[RhizValue], _: &Path) -> ExecutionResult {
     error_with!("Execution aborted")
 }
 
+/// Delete a file (by absolute path, or path relative to the Rhizfile).
 fn delete_file(args: &[RhizValue], working_dir: &Path) -> ExecutionResult {
     use std::fs;
     assert!(working_dir.is_dir());
@@ -63,11 +84,53 @@ fn delete_file(args: &[RhizValue], working_dir: &Path) -> ExecutionResult {
     } else {
         error_with!("`delete-file` takes a string");
     };
-    let target_path = {
-        let mut pbuf = working_dir.to_path_buf();
-        pbuf.push(fpath);
-        pbuf
-    };
+    let target_path = join_cwd(working_dir, fpath);
+
     fs::remove_file(target_path)?;
+    Ok(())
+}
+
+fn delete_dir(args: &[RhizValue], working_dir: &Path) -> ExecutionResult {
+    assert!(working_dir.is_dir());
+    check_args_len!("delete-dir", args, 1);
+    let dpath = if let RhizValue::String(dpath) = &args[0] {
+        dpath
+    } else {
+        error_with!("`delete-dir` takes a string");
+    };
+    let target_path = join_cwd(working_dir, dpath);
+    if !target_path.is_dir() {
+        error_with!("`delete-dir` is for deleting directories");
+    }
+
+    std::fs::remove_dir_all(&target_path)?;
+
+    Ok(())
+}
+
+fn exec(args: &[RhizValue], working_dir: &Path) -> ExecutionResult {
+    assert!(working_dir.is_dir());
+    if args.is_empty() {
+        error_with!("`exec` needs at least one argument");
+    }
+    let cmd_name = val_to_string(&args[0]).ok_or(ExecutionError::from(
+        "`exec` takes a string or symbol as a command name",
+    ))?;
+
+    let mut cmd = Command::new(&cmd_name);
+    if args.len() > 1 {
+        let mut cmd_args = Vec::new();
+        for arg in args.iter().skip(1) {
+            match val_to_string(arg) {
+                Some(s) => cmd_args.push(s),
+                None => error_with!("`exec` takes strings or symbols as command arguments"),
+            }
+        }
+        cmd.args(&cmd_args);
+    }
+    cmd.current_dir(working_dir);
+    let mut child_process = cmd.spawn()?;
+    child_process.wait()?;
+
     Ok(())
 }
